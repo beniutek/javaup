a18935c3e047396c82eee0271eeb26872f36b299
Beniamin Bartkowiak <beniamin.bartkowiak@boardintelligence.co.uk>
2019-09-13 11:07:30 +0200

    kolos poprawkowy

--- nauka/src/com/company/Main.java
+++ nauka/src/com/company/Main.java
@@ -8,32 +8,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
 
-//        Person jan = new Person("JAN", "KOWALSKI", 300, "m");
-//        Person marian = new Person("MARIAN", "KOWALSKI", 4000, "m");
-//        Person rysiek = new Person("Rychu", "Peja", 0.50, "f");
-//        Person zenon = new Person("Zenon", "Martyniuk", 299.99, "m");
-//
-//        System.out.println("PERSONA " + jan.toString());
-//        Person[] perArr = new Person[4];
-//        perArr[3] = jan;
-//        perArr[2] = marian;
-//        perArr[1] = rysiek;
-//        perArr[0] = zenon;
-//
-//        Arrays.sort(perArr);
-//        System.out.println("default (comparable) sort: \n");
-//        for(Person i : perArr) {
-//            System.out.println(i);
-//        }
-//
-//        Arrays.sort(perArr, new PersonComparator());
-//        System.out.println("salary (comparator) sort: \n");
-//        for(Person i : perArr) {
-//            System.out.println(i);
-//        }
-//
-//        Person klon = zenon.clone();
-//        System.out.println("\nKLON: " + klon);
+        Person jan = new Person("JAN", "KOWALSKI", 300, "m");
+        Person marian = new Person("MARIAN", "KOWALSKI", 4000, "m");
+        Person rysiek = new Person("Rychu", "Peja", 0.50, "f");
+        Person zenon = new Person("Zenon", "Martyniuk", 299.99, "m");
+
+        System.out.println("PERSONA " + jan.toString());
+        Person[] perArr = new Person[4];
+        perArr[3] = jan;
+        perArr[2] = marian;
+        perArr[1] = rysiek;
+        perArr[0] = zenon;
+
+        Arrays.sort(perArr);
+        System.out.println("default (comparable) sort: \n");
+        for(Person i : perArr) {
+            System.out.println(i);
+        }
+
+        Arrays.sort(perArr, new PersonComparator());
+        System.out.println("salary (comparator) sort: \n");
+        for(Person i : perArr) {
+            System.out.println(i);
+        }
+
+        Person klon = zenon.clone();
+        System.out.println("\nKLON: " + klon);
 
         File file = new File("test.txt");
         Scanner scanner = null;
--- wyjatki/src/Miasto.java
+++ wyjatki/src/Miasto.java
@@ -1,2 +1,21 @@
 public class Miasto {
+    private String nazwaMiasta;
+    private String prezydentMiasta;
+
+    public Miasto() {
+      this.Miasto("Krakow", "Chujowy");
+    }
+
+    public Miasto(String nazwaMiasta, String prezydentMiasta) {
+        this.nazwaMiasta = nazwaMiasta;
+        this.prezydentMiasta = prezydentMiasta;
+    }
+
+    @Override
+    public String toString() {
+        return "Miasto {" +
+                "nazwaMiasta='" + nazwaMiasta + '\'' +
+                ", prezydentMiasta='" + prezydentMiasta + '\'' +
+                " }";
+    }
 }
