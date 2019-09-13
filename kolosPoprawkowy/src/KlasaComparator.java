d4698c37ba5a59fe19fc83d2b352873d85974e35
Beniamin Bartkowiak <beniamin.bartkowiak@boardintelligence.co.uk>
2019-09-13 13:15:06 +0200

    wip

--- kolosPoprawkowy/src/KlasaComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class KlasaComparator implements Comparator<Klasa1> {
-        @Override
-        public int compare(Klasa1 k1, Klasa1 k2) {
-            return k1.getCalkowita() - k2.getCalkowita();
-        }
-}
