0e430b8b5ec268741a771a8444520f8f60495d28
Beniamin Bartkowiak <beniamin.bartkowiak@boardintelligence.co.uk>
2019-09-13 13:10:36 +0200

    fix

--- kolosPoprawkowy/src/Klasa2.java
+++ kolosPoprawkowy/src/Klasa2.java
@@ -3,45 +3,23 @@ import java.util.Arrays;
 // zad 1
 // zad 2 -> final
 public final class Klasa2 extends Klasa1 {
-    private double[][] tabDouble;
-    private int calkowita;
     private double rzeczywita;
 
     public Klasa2() {
-        this.calkowita = 0;
+        super();
         this.rzeczywita = 0.0;
-        this.tabDouble = new double[1][1];
     }
 
     public Klasa2(double[][] tabDouble, int calkowita, double rzeczywita) {
-        this.tabDouble = tabDouble;
-        this.calkowita = calkowita;
+        super(tabDouble, calkowita);
         this.rzeczywita = rzeczywita;
     }
 
     @Override
     public String toString() {
         return "Klasa2{" +
-                "tabDouble=" + Arrays.toString(tabDouble) +
-                ", calkowita=" + calkowita +
-                ", rzeczywita=" + rzeczywita +
-                '}';
-    }
-
-    public double[][] getTabDouble() {
-        return tabDouble;
-    }
-
-    public void setTabDouble(double[][] tabDouble) {
-        this.tabDouble = tabDouble;
-    }
-
-    public int getCalkowita() {
-        return calkowita;
-    }
-
-    public void setCalkowita(int calkowita) {
-        this.calkowita = calkowita;
+                "rzeczywita=" + rzeczywita +
+                "} " + super.toString();
     }
 
     public double getRzeczywita() {
